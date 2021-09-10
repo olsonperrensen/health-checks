0d9facef9fd9e9872635469e9bad73233adde814
Equipboard Submission <htcevo4gnougat@gmail.com>
2021-09-10 19:43:26 +0200

    GNU License 2.0

--- health_check.py
+++ health_check.py
@@ -1,5 +1,12 @@
 # Made possible thanks to the GNU open-source project.
-
+#The first software-sharing community
+#When I started working at the MIT Artificial Intelligence Lab in 1971, I #became part of a software-sharing community that had existed for #many years. Sharing of software was not limited to our particular #community; it is as old as computers, just as sharing of recipes is as #old as cooking. But we did it more than most.
+#
+#The AI Lab used a timesharing operating system called ITS (the #Incompatible Timesharing System) that the lab's staff hackers (1) had #designed and written in assembler language for the Digital PDP-10, #one of the large computers of the era. As a member of this #community, an AI Lab staff system hacker, my job was to improve this #system.
+#
+#We did not call our software “free software”, because that term did not #yet exist; but that is what it was. Whenever people from another #university or a company wanted to port and use a program, we gladly #let them. If you saw someone using an unfamiliar and interesting #program, you could always ask to see the source code, so that you #could read it, change it, or cannibalize parts of it to make a new #program.
+#
+#(1) The use of “hacker” to mean “security breaker” is a confusion on #the part of the mass media. We hackers refuse to recognize that #meaning, and continue using the word to mean someone who loves #to program, someone who enjoys playful cleverness, or the #combination of the two. See my article, On Hacking.
 This is one file that has been created locally from one of the contributor's
 computers
 
